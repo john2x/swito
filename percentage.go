f22597a59053152f31968ac47147ebfd25c21449
John Louis Del Rosario <john2x@gmail.com>
2022-08-13 22:01:44 +1200

    Fix regex for x% of y

--- percentage.go
+++ percentage.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	xPercentOfYRegex = regexp.MustCompile(`(?P<x>\d+)% of (?P<y>\d+)$`) // e.g. 10% of 100
+	xPercentOfYRegex = regexp.MustCompile(`^(?P<x>([0-9]*[.])?[0-9]+)% of (?P<y>[-]?([0-9]*[.])?[0-9]+)$`) // e.g. 10% of 100
 	xOverYInPercent  = regexp.MustCompile(`(?P<x>\d+)% of (?P<y>\d+)$`) // e.g. 10% of 100
 	xToYInPercent    = regexp.MustCompile(`(?P<x>\d+)% of (?P<y>\d+)$`) // e.g. 10% of 100
 
@@ -20,14 +20,14 @@ var (
 func answer(inputs []string) (string, error) {
 	prompt := strings.Join(inputs, " ")
 
-	if m := xPercentOfYRegex.FindStringSubmatch(prompt); len(m) == 2 {
+	if m := xPercentOfYRegex.FindStringSubmatch(prompt); len(m) == 5 {
 		x, xErr := strconv.ParseFloat(m[1], 2)
-		y, yErr := strconv.ParseFloat(m[0], 2)
+		y, yErr := strconv.ParseFloat(m[3], 2)
 		if xErr != nil || yErr != nil {
 			return "", errors.New(defaultErrorMessage)
 		}
 		result := x * y * 0.01
-		answer := fmt.Sprintf("%s%% of %s is %f", inputStyle.Render(m[0]), inputStyle.Render(m[1]), answerStyle.Render(fmt.Sprintf("%f", result)))
+		answer := fmt.Sprintf("%s%% of %s is %s", inputStyle.Render(m[1]), inputStyle.Render(m[3]), answerStyle.Render(fmt.Sprintf("%.2f", result)))
 		return answer, nil
 	} else {
 		return "", errors.New(errorStyle.Render("Unable to parse the question."))
